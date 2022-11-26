208ad229cfdce0a1b54f65af9369a0474770bc13
ernestmusser88 <ernestmusseriii@gmail.com>
2022-11-25 19:53:33 -0500

    Temporary changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -6,9 +6,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#apikey = os.environ['apikey']
-#url = os.environ['url']
-
 authenticator = IAMAuthenticator('MY3ZS8siRrGUydE6RtLCw7c5uRvFARrELVDT6ossi86Q')
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
@@ -16,23 +13,34 @@ language_translator = LanguageTranslatorV3(
 )
 #set service url to US South Dallas
 language_translator.set_service_url('https://api.us-south.language-translator.watson.cloud.ibm.com/instances/6550b837-7d26-43e8-b6c6-aa7345fe2741')
-# disabling SSL for self signed cert
-language_translator.set_disable_ssl_verification(True)
 
 #English to French 
 def englishToFrench(englishText):
-    #write the code here
-    frenchText = language_translator.translate(
-    text= englishText,
-    model_id='en-fr').get_result()
+    
+    if englishText != None:
+        #Call Translation Service
+        translation = language_translator.translate(
+        text= englishText,
+        model_id='en-fr').get_result()
+        #Access translated response
+        frenchText = translation['translations'][0]['translation']
+        return frenchText
+    else:
+        return None
 
-    return frenchText
+    
 
 #French to English
 def frenchToEnglish(frenchText):
-    #write the code here
-    englishText = language_translator.translate(
-    text= frenchText,
-    model_id='fr-en').get_result()
     
-    return englishText
+    if frenchText != None:
+        #Call Translation Service
+        translation = language_translator.translate(
+        text= frenchText,
+        model_id='fr-en').get_result()
+        #Access translated response
+        englishText = translation['translations'][0]['translation']
+        return englishText
+    else:
+        return None
+
