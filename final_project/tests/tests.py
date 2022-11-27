cdb96113c73dda390b2019490308a2a977429e97
ernestmusser88 <ernestmusseriii@gmail.com>
2022-11-27 17:37:35 -0500

    Final Changes

--- final_project/tests/tests.py
+++ final_project/tests/tests.py
@@ -1,13 +1,12 @@
 import unittest
 
 #from translator import english_to_french, french_to_english
-from machinetranslation import translator
+from machinetranslation.translator import english_to_french, french_to_english
 
 class TestMyFunc(unittest.TestCase):
     def test_french_to_english(self):
         self.assertEqual(french_to_english(None),None)
         self.assertEqual(french_to_english('Bonjour'),'Hello')
-        self.assertEqual(french_to_english(None),'Soup')
 
     def test_english_to_french(self):
         self.assertEqual(english_to_french(None),None)
